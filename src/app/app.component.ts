9014ff6605c0607425e11333de2a572acb20dd93
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-06-12 13:51:25 +0200

    beta release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
@@ -11,7 +10,7 @@ export class AppComponent implements OnInit {
   title = 'debitspace';
   unsupported = false;
 
-  constructor(private router: Router, public breakpointObserver: BreakpointObserver) { }
+  constructor(public breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.checkScreenSize();
