1ba87c944c54048340d06ed05a378d110ed433f0
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-06-13 14:57:11 +0200

    indexdb setup

--- src/app/services/workspaces.service.ts
+++ src/app/services/workspaces.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WorkspacesService {
 
-  constructor() { }
+export class WorkspacesService extends BaseService {
+  constructor() {
+    super();
+  }
 }
